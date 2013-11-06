6c5b9e86dad65b722c1566522b012dc42d29dda2
仲若重人 <ugo.nakawaka:gmail.com>
2013-11-06 14:29:55 +0900

    first

---
+++ xcode/aaa/aaa/AaaViewController.m
@@ -0,0 +1,29 @@
+//
+//  AaaViewController.m
+//  aaa
+//
+//  Created by nakawaka shigeto on 2013/11/06.
+//  Copyright (c) 2013年 deiji.jp. All rights reserved.
+//
+
+#import "AaaViewController.h"
+
+@interface AaaViewController ()
+
+@end
+
+@implementation AaaViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
