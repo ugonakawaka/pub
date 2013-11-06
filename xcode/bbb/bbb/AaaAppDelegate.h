53f5e80b752291d050691dcc6c0bdff230bc85b6
仲若重人 <ugo.nakawaka:gmail.com>
2013-11-06 14:44:02 +0900

    2
    2

---
+++ xcode/bbb/bbb/AaaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AaaAppDelegate.h
+//  bbb
+//
+//  Created by nakawaka shigeto on 2013/11/06.
+//  Copyright (c) 2013年 deiji.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AaaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xcode/bbb/bbb/AaaViewController.h
@@ -0,0 +1,13 @@
+//
+//  AaaViewController.h
+//  bbb
+//
+//  Created by nakawaka shigeto on 2013/11/06.
+//  Copyright (c) 2013年 deiji.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AaaViewController : UIViewController
+
+@end
