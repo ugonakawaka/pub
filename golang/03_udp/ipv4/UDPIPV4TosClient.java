627b41c03e8940ede8ff1082e6d6047a64b3446f
ugonakawaka <ugo.nakawaka@gmail.com>
2020-12-21 00:59:24 +0900

    add

---
+++ golang/03_udp/ipv4/UDPIPV4TosClient.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class UDPIPV4TosClient {
+
+	interface TOS {
+
+		final int IPTOS_LOWCOST = (0x02);
+		final int IPTOS_RELIABILITY = (0x04);
+		final int IPTOS_THROUGHPUT = (0x08);
+		final int IPTOS_LOWDELAY = (0x10);
+
+	}
+
+	public static void main(String[] args) throws UnknownHostException, SocketException {
+
+		final int port = 55501;
+		final InetAddress laddr = InetAddress.getByName("192.168.1.6");
+
+		final DatagramSocket socket = new DatagramSocket();
+
+		final IntStream intStream = IntStream.range(1, 101/* exclusive */);
+
+		// final int[] tos = { TOS.IPTOS_LOWCOST, TOS.IPTOS_RELIABILITY,
+		// TOS.IPTOS_THROUGHPUT, TOS.IPTOS_LOWDELAY, };
+		intStream.forEach(i -> {
+			System.out.print(".");
+			byte[] buf = ("" + i).getBytes();
+			final DatagramPacket p = new DatagramPacket(buf, buf.length, laddr, port);
+			final int j = new Random(System.nanoTime()).nextInt(256/* exclusive */);
+
+			try {
+				socket.setTrafficClass(j/* 0 <= tc <= 255 */);
+				socket.send(p);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if ((i % 30) == 0)
+				System.out.println();
+		});
+
+	}
+
+}
