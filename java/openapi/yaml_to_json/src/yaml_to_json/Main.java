9d155f76f3a2f64689de20da74dfefa530b95525
shigeto nakawaka <nakawaka@gmail.com>
2021-12-05 17:40:55 +0900

    update

--- java/openapi/yaml_to_json/src/yaml_to_json/Main.java
+++ java/openapi/yaml_to_json/src/yaml_to_json/Main.java
@@ -1,11 +1,13 @@
 package yaml_to_json;
 
-import java.io.File;
+
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,6 +26,10 @@ public class Main {
 
 		final Path p = Paths.get("spec/");
 
+		final Path outputdir = Paths.get("json/");
+
+		outputdir.toFile().mkdir();
+
 		System.out.println(p.toUri());
 
 		final ObjectMapper objectMapper = new ObjectMapper();
@@ -31,10 +37,13 @@ public class Main {
 		try (DirectoryStream<Path> stream = Files.newDirectoryStream(p)) {
 
 			stream.forEach(a -> {
-				System.out.println(a);
+				// System.out.println(a);
+
+				final String filenameBase = a.toFile().getName().split("\\.", 2)[0];
+				System.out.println(filenameBase);
 
 				try {
-					// String read = Files.readAllLines(a).toString();
+		
 					final Stream<String> lines = Files.lines(a);
 					final String s = lines.collect(Collectors.joining("\n"));
 					lines.close();
@@ -42,7 +51,11 @@ public class Main {
 					final Map<String, Object> obj = yaml.load(s);
 
 					final String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
-					System.out.println(json);
+					// System.out.println(json);
+					
+					Files.writeString(Paths.get("json/", filenameBase + ".json"), json, Charset.forName("UTF-8"),
+							StandardOpenOption.CREATE);
+
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
