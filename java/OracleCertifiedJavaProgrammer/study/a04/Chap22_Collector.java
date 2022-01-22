f2e4a7e502bb3ad39c8cd796d47d91432913098e
shigeto nakawaka <nakawaka@gmail.com>
2022-01-23 00:20:03 +0900

    add

---
+++ java/OracleCertifiedJavaProgrammer/study/a04/Chap22_Collector.java
@@ -0,0 +1,85 @@
+package a04;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class Chap22_Collector {
+
+	public static class Joinner implements Collector<String, StringBuilder, String> {
+
+		@Override
+		public Supplier<StringBuilder> supplier() {
+			return StringBuilder::new;
+		}
+
+		@Override
+		public BiConsumer<StringBuilder, String> accumulator() {
+			return (sb, b) -> {
+				sb.append(b).append(",");
+			};
+		}
+
+		@Override
+		public BinaryOperator<StringBuilder> combiner() {
+			return (a, b) -> a.append(b);
+		}
+
+		@Override
+		public Function<StringBuilder, String> finisher() {
+			return (sb) -> {
+				if (0 < sb.length() ) {
+					return sb.substring(0, sb.length() - 1);
+				}
+				return sb.toString();
+			};
+		}
+
+		@Override
+		public Set<Characteristics> characteristics() {
+			return Collections.emptySet();
+			// return EnumSet.of(Characteristics.UNORDERED);
+			// return EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.UNORDERED);
+		}
+
+	}
+
+	static final List<String> list = Arrays.asList("A", "B", "C", "D", "E");
+
+	static void sleep() {
+		// Zzzz...
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// do nothing
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		{ // empty list 
+			var result = new ArrayList<String>().stream().collect(new Joinner());
+
+			System.out.println(result);
+		}
+		{ // test  BiConsumer
+			var result = list.stream().collect(new Joinner());
+
+			System.out.println(result);
+		}
+		{ // test BinaryOperator
+			var result = list.stream().parallel().collect(new Joinner());
+			System.out.println(result);
+		}
+		
+	}
+
+}
