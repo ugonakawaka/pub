9db464d1a37d70b9b498ed10e1cda7e26a8081f6
shigeto nakawaka <nakawaka@gmail.com>
2022-05-29 14:48:52 +0900

    update

--- java/OracleCertifiedJavaProgrammer/src/main/java/a01_basic/ChapXx11_bitmask.java
+++ java/OracleCertifiedJavaProgrammer/src/main/java/a01_basic/ChapXx11_bitmask.java
@@ -1,6 +1,7 @@
 package a01_basic;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ChapXx11_bitmask {
 
@@ -30,7 +31,22 @@ public class ChapXx11_bitmask {
 			final int val = 0x1234;
 			System.out.println(val);// 4660
 			List.of(bitmasks).stream().map(b -> val & b).forEach(System.out::println);
+		}
+		System.out.println("=======");
+		{
+			
+			var print = new Consumer<Integer>() {
 
+				@Override
+				public void accept(Integer t) {
+					System.out.printf("0x%x ", t);
+					
+				}
+			};
+			final Integer[] bitmasks = { 0xF, 0x0000, 0xFFFF, 0x000F, 0xF000, 0x0F00, 0x00F0 };
+			final int val = 0x1234;
+			System.out.println(val);// 4660
+			List.of(bitmasks).stream().peek(print).map(b -> val & b).forEach(System.out::println);
 		}
 	}
 }
