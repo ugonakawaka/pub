3ed5f3cfb55deb924a84b6aca2581a946f7d5b9b
shigeto nakawaka <nakawaka@gmail.com>
2022-12-17 15:09:51 +0900

    update

--- swift/a00_basic/main.swift
+++ swift/a00_basic/main.swift
@@ -1,2 +1,30 @@
+
+
 print(1)
 print("ok")
+print("ok")
+
+// クロージャー
+aaa: do {
+    var c1 = { () in print("hello") } // これがいいかなーどうだろう
+    var c2 = { () in print("hello") }
+    var c3 = { () in print("hello") }
+    var c4 = { print("hello") } // 省略しまくり
+    c1()
+    c2()
+    c3()
+    c4()
+
+    // ==================
+    let d1 = { print("即hello"); return "ok" }() // 即時実行して値を
+    print("値を返す場合は、即時実行できるぽい \(d1)")
+}
+
+// クロージャー
+bbb: do {
+    var CONST1 = "o_o!"
+    var c1 = { print("hello \(CONST1)") } // 外側の値を使う
+    c1()
+	CONST1 = ">_<!"
+	c1()
+}
--- swift/a00_basic/memo.md
+++ swift/a00_basic/memo.md
@@ -1,3 +1,19 @@
+### コンパイルするよ
+```
 swiftc main.swift 
+```
 
+### 即時実行
+```
 swift main.swift
+```
+
+### swftformt
+```
+brew install swiftformat
+swiftformat main.swift
+```
+### ワーニングをおさえるよ  
+```
+-suppress-warnings      Suppress all warnings  
+```
