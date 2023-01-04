665afb92fa958442606a5951b50877e66e95b955
shigeto nakawaka <nakawaka@gmail.com>
2023-01-04 23:25:37 +0900

    ok

---
+++ swift/a00_basic/execMain.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# SWIFT_VERSION=$(swift -version)
+SWIFT_VERSION=5.7.2
+MAIN=main
+
+echo $SWIFT_VERSION
+echo $MAIN
+rm $MAIN
+cmd1="swiftformat $1.swift --swiftversion $SWIFT_VERSION"
+cmd2="swiftc $1.swift -o $MAIN"
+echo "##########################"
+$cmd1 && $cmd2 && {
+  ./$MAIN
+}
+echo "##########################"
+
