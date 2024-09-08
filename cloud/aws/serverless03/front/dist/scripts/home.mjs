0b9dc301cfac3a959946214617f8e855e18066fc
shigeto nakawaka <nakawaka@gmail.com>
2024-09-08 15:54:15 +0900

    douda

--- cloud/aws/serverless03/front/dist/scripts/home.mjs
+++
@@ -1,9 +0,0 @@
-import { auth } from "./auth.mjs";
-
-export function checkAuthOnHome() {
-  if (window.location.pathname === config.routes.home) {
-    if (!auth.isAuthenticated()) {
-      auth.redirectToLogin();
-    }
-  }
-}
