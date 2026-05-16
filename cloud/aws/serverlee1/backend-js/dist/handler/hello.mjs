ccb18ca02c7d26740734cc8d17bc0530c374aa79
shigeto nakawaka <nakawaka@gmail.com>
2026-05-16 10:15:56 +0900

    Delete cloud/aws/serverlee1 directory

--- cloud/aws/serverlee1/backend-js/dist/handler/hello.mjs
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-    // ここにロジックを記述します
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Hello, world!",
-        }),
-    };
-};
