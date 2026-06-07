d01025ef2a35d9bfd21cfe736de9259066b24e0a
shigeto nakawaka <nakawaka@gmail.com>
2026-06-07 12:54:43 +0900

    Delete cloud/aws/serverless03 directory

--- cloud/aws/serverless03/front/src/scripts/main.mjs
+++
@@ -1,8 +0,0 @@
-import { setupLoginForm } from "./login.mjs";
-import { checkAuthOnHome } from "./home.mjs";
-import { apiCheck } from "./api-check.mjs";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupLoginForm();
-  checkAuthOnHome();
-});
